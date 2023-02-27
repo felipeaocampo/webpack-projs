5cf538ba9775cd09384cf711603a22a817bdf7ee
felipeaocampo <felipe.aocampo@gmail.com>
2023-02-26 23:56:16 -0500

    working on bundling together files

---
+++ src/add-image.js
@@ -0,0 +1,13 @@
+import Alien from './ackbar.jpg';
+import altText from './altText.txt';
+
+function addImage() {
+  const img = document.createElement('img');
+  img.alt = altText;
+  img.width = 300;
+  img.src = Alien;
+  const body = document.querySelector(`body`);
+  body.appendChild(img);
+}
+
+export default addImage;
---
+++ src/components/hello-world-button/hello-world-button.js
@@ -0,0 +1,24 @@
+import './hello-world-button.scss';
+
+class HelloWorldButton {
+  buttonCssClass = 'hello-world-button';
+  render() {
+    const button = document.createElement(`button`);
+    button.innerHTML = `Hello World`;
+    const body = document.querySelector('body');
+
+    //STYLING
+    button.classList.add(this.buttonCssClass);
+
+    //EVENT LISTENERS
+    button.onclick = function () {
+      const p = document.createElement(`p`);
+      p.innerHTML = 'Hello world';
+      p.classList.add(`hello-world-text`);
+      body.appendChild(p);
+    };
+    body.appendChild(button);
+  }
+}
+
+export default HelloWorldButton;
